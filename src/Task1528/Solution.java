8e500d61f07ab29d486f49eb8e8571f887fab630
Pavel <lp.qroovemaster@gmail.com>
2023-02-06 10:50:57 +0300

    Task 1528. Shuffle String https://leetcode.com/problems/shuffle-string/

--- src/Task1528/Solution.java
+++ src/Task1528/Solution.java
@@ -1,2 +1,15 @@
-package Task1528;public class Solution {
+package Task1528;
+
+class Solution {
+    public String restoreString(String s, int[] indices) {
+
+        char[] symbols = s.toCharArray();
+        char[] newSymbols = new char[indices.length];
+
+        for (int i = 0; i < indices.length; i++) {
+            newSymbols[indices[i]] = symbols[i];
+        }
+
+        return new String(newSymbols);
+    }
 }
--- src/Task1528/SolutionDemo.java
+++ src/Task1528/SolutionDemo.java
@@ -1,2 +1,21 @@
-package Task1528;public class SolutionDemo {
+package Task1528;
+
+import java.util.Arrays;
+
+public class SolutionDemo {
+
+    public static void main(String[] args) {
+        String testString = "codeleet";
+        int[] testIndices = new int[] {4,5,6,7,0,2,1,3};
+
+        Solution solution = new Solution();
+
+        String result = solution.restoreString(testString, testIndices);
+
+        System.out.println("In:                 " + testString);
+        System.out.println("Indexes to replace: " + Arrays.toString(testIndices));
+        System.out.println("Out:                " + result);
+
+    }
+
 }
